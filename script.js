78e348b32a8cb72a684a1b382f9cef3b72178397
Kirillba2011 <kirillba2011@gmail.com>
2026-05-09 14:40:19 +0300

    dz

--- script.js
+++ script.js
@@ -1,18 +1,17 @@
 const store = document.querySelector(".things-to-buy")
-let currentPrice = document.querySelector(".currentPrice")
+let currentValue = document.querySelector(".currentValue")
 let add = 0
 let amount = 0
 store.addEventListener('click', function(event){
     let target = event.target
     let price = Number(target.getAttribute("price"))
-    if (target.classList.price)
+    if (target.hasAttribute("price"))
         if (price > 0){
             amount += 1
             add += price
-            currentPrice.textContent = add}
+            currentValue.textContent = add}
         else if(amount > 0){
             amount -= 1
             add += price
-            currentPrice.textContent = add
-    }
+            currentValue.textContent = add}
 })
