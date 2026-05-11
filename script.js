92d3b434c9a8d7400a1fbe120960cb4b83cee731
Kirillba2011 <kirillba2011@gmail.com>
2026-05-11 09:53:47 +0300

    dz

--- script.js
+++ script.js
@@ -7,16 +7,21 @@ store.addEventListener("click", function(event){
     if (price > 0){
         let num = Number(target.getAttribute("number"))
         let minBtn = store.querySelectorAll(".minBtn")[num]
+        let currentAmount = store.querySelectorAll(".currentAmount")[num]
         let amount = Number(minBtn.getAttribute("amount"))
         amount += 1
         add += price
         currentValue.textContent = add
+        currentAmount.textContent = amount
         minBtn.setAttribute("amount", amount)}
     else{
         let amount = Number(target.getAttribute("amount"))
         if (amount > 0){
+            let num = Number(target.getAttribute("number"))
+            let currentAmount = store.querySelectorAll(".currentAmount")[num]
             amount -= 1
             add += price
             currentValue.textContent = add
+            currentAmount.textContent = amount
             target.setAttribute('amount', amount)}}
 })
